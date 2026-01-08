70c7ba14cb2c4a049d6e5d60188efd9ea16980aa
webApp2 <ashishmca1202@gmail.com>
2026-01-08 22:14:41 +0530

    Fetchbconnection String Value from key vault

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
+using Azure.Identity;
 using Microsoft.EntityFrameworkCore;
+using System;
 using WebApplication1.Models;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("EFCoreDBConnection"));
+//});
+
+//var app = builder.Build();
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// Key Vault URL
+var keyVaultUrl = new Uri("https://WebProject1Vault.vault.azure.net/");
+// Authenticate using Managed Identity
+var secretClient = new SecretClient(
+    keyVaultUrl,
+    new DefaultAzureCredential());
+// Get secret
+KeyVaultSecret secret = secretClient.GetSecret("my-first-azure-sql-server");
+string connectionString = secret.Value;
+// Use connection string
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("EFCoreDBConnection"));
-});
-
+    options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
