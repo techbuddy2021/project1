e7a4aec6b3ccb4fab20b71b30ef274c8ccc372e5
webApp2 <ashishmca1202@gmail.com>
2026-01-08 23:22:57 +0530

    Updated1 Fetch connection String Value from key vault1

--- Program.cs
+++ Program.cs
@@ -18,8 +18,8 @@ using Azure.Security.KeyVault.Secrets;
 
 var builder = WebApplication.CreateBuilder(args);
 // Key Vault URL
-var keyVaultUrl = new Uri("https://WebProject1Vault.vault.azure.net/");
-// Authenticate using Managed Identity in Azure
+var keyVaultUrl = new Uri("https://webproject1vault.vault.azure.net/");
+// Authenticate using Managed Identity in Azu
 var secretClient = new SecretClient(
     keyVaultUrl,
     new DefaultAzureCredential());
